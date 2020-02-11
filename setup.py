09724f2586cf3c72c15f8f6d2a64a9f5bffb495f
Jesse van Leeuwen <sf.jessenl@gmail.com>
2020-02-11 23:59:14 +0100

    added execution files added refs in the package setup

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@
 from setuptools import setup
 
 setup(name='meshroombot',
-      version='0.1',
+      version='0.1.1',
       description='The funniest joke in the world',
       url='https://github.com/JesseLeeuwen/meshroombot.git',
       author='Jesse van Leeuwen',
       author_email='jesse@emberglitch.com',
       license='MIT',
       packages=['meshroombot'],
+      scripts=['bin/meshroombot-run', 'bin/meshroombot-setup'],
       zip_safe=False)
