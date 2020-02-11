7a158a1e64e31663b86419cc5a2845757c032e41
Jesse van Leeuwen <sf.jessenl@gmail.com>
2020-02-11 22:33:57 +0100

    initial package commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+
+setup(name='meshroombot',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/JesseLeeuwen/meshroombot.git',
+      author='Jesse van Leeuwen',
+      author_email='jesse@emberglitch.com',
+      license='MIT',
+      packages=['meshroombot'],
+      zip_safe=False)
